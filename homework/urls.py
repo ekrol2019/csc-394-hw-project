41e953f104436169a288f4dfab2f2cc675d2b673
ekrol2019 <115721611+ekrol2019@users.noreply.github.com>
2023-04-18 23:26:37 -0500

    Update urls.py

--- homework/urls.py
+++ homework/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path("student", include('students.urls')),
     path('admin/', admin.site.urls)
+    path('blog/', include('blog.urls'))
 ]
