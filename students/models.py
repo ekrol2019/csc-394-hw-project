88c378151df4619c5b76f9144a6f2c404a8156dc
ekrol2019 <ekrol2@depaul.edu>
2023-04-18 22:31:50 -0500

    s

--- students/models.py
+++ students/models.py
@@ -10,11 +10,11 @@ class Student(models.Model):
 
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
-    
+
 class Course(models.Model):
     department = models.CharField(max_length=60)
     number = models.CharField(max_length=3)
-    students = models.ManyToManyField(Student)
+    students = models.ManyToManyField(Student, related_name='student_courses')
 
     def __str__(self) -> str:
         return self.department + "-" + self.number
